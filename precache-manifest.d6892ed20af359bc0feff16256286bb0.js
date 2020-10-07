3b63cac3aec1b69c86b9c9cf49e2cc0d432748cc
minh100 <minh100@live.unc.edu>
2020-10-07 02:35:53 -0400

    Updates

---
+++ precache-manifest.d6892ed20af359bc0feff16256286bb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565cc892250f2fc2e79ab4f98a29b09a",
+    "url": "/MyList/index.html"
+  },
+  {
+    "revision": "321d54d6eaa2e9ba5927",
+    "url": "/MyList/static/css/main.024fcd47.chunk.css"
+  },
+  {
+    "revision": "27b2becc6d3c220b2245",
+    "url": "/MyList/static/js/2.e2bf512e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyList/static/js/2.e2bf512e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321d54d6eaa2e9ba5927",
+    "url": "/MyList/static/js/main.8d5d36f9.chunk.js"
+  },
+  {
+    "revision": "946d2d13c891cf612bcd",
+    "url": "/MyList/static/js/runtime-main.9165ed2e.js"
+  }
+]);
