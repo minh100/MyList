90edfa8c1f8d63662e4871c19d8bca65d2d94a88
minh100 <minh100@live.unc.edu>
2020-10-07 20:51:07 -0400

    fixed responsiveness for viewing more

--- build/precache-manifest.ac7896594827836fc23d544edaf454d5.js
+++ build/precache-manifest.ac7896594827836fc23d544edaf454d5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6a1a3bafdc6b529b997de0a60abcbce",
+    "revision": "10baaf0db5196ec360a28a187918ccf8",
     "url": "/MyList/index.html"
   },
   {
-    "revision": "1334d7cff7528d3241d1",
-    "url": "/MyList/static/css/main.79b763c3.chunk.css"
+    "revision": "24d0e4d53e1e818afdc3",
+    "url": "/MyList/static/css/main.d1f6bce2.chunk.css"
   },
   {
     "revision": "27b2becc6d3c220b2245",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MyList/static/js/2.e2bf512e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1334d7cff7528d3241d1",
+    "revision": "24d0e4d53e1e818afdc3",
     "url": "/MyList/static/js/main.11c851a6.chunk.js"
   },
   {
